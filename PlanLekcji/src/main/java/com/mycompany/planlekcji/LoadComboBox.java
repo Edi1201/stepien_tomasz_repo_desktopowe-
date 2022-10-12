efb2f5feb5dc7c02cfbcae625023f829c4be407b
Edi1201 <77607534+Edi1201@users.noreply.github.com>
2022-10-12 13:16:35 +0200

    gvvdfds

---
+++ PlanLekcji/src/main/java/com/mycompany/planlekcji/LoadComboBox.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.planlekcji;
+
+import java.io.File;
+import javax.swing.JComboBox;
+
+/**
+ *
+ * @author tomaszstepien
+ */
+public class LoadComboBox {
+    public void load(String name, JComboBox comboBox) {
+        
+    File folder = new File("data/"+name+"/");
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                String fileName = listOfFiles[i].getName();
+                comboBox.addItem(fileName.substring(0, fileName.length()-4));
+//                System.out.println("File " + listOfFiles[i].getName());
+            } 
+        } 
+    }
+}
---
+++ PlanLekcji/src/main/java/com/mycompany/planlekcji/ZmianaPlanu.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.planlekcji;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author tomaszstepien
+ */
+public class ZmianaPlanu {
+    public void changePlan(JTable table, String planType, String planTypeValue) {
+        // planType = ""
+        File f = new File("data/"+planType+"/"+planTypeValue+".csv");
+        String data = "";
+      
+        // clears table before inserting values in case there would be some in blank spots
+        clearTableValues(table); 
+        
+        try {
+            Scanner sc = new Scanner(f);
+            while(sc.hasNextLine()) {
+                data += sc.nextLine();
+                if(sc.hasNextLine()) data += "\n";
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PlanLekcji.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        String[] days = data.split("\n");
+        
+        for(int i = 0; i < days.length; i++) {
+            String[] lessons = days[i].split(";");
+            for(int j = 0; j < lessons.length; j++) {
+                String[] lessonsSpecifics = lessons[j].split(":");
+
+                String val1 = lessonsSpecifics[0];
+                String val2 = lessonsSpecifics[1];
+                String val3 = lessonsSpecifics[2];
+                
+                DefaultTableModel model = new DefaultTableModel();
+                table.setValueAt(val1+" "+val2+" "+val3, j, i+2);
+//                jTable1.setVa
+                //todo wszytkso
+                
+//                System.out.println(""+lessonName);
+
+            }
+        }
+    }
+    
+    private void clearTableValues(JTable table) {
+        for(int i = 1; i < 9; i++) {
+            for(int j = 2; j < 7; j++) {
+                table.setValueAt("", i, j);
+            }
+        }
+    }
+}
